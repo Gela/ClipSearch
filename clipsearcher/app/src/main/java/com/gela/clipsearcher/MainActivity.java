9446a08efd414b78b0a66319274d8a4a14c82e78
Gela <gela.malekpour@gmail.com>
2024-02-23 15:00:58 -0800

    Disable search button if input field is empty

--- clipsearcher/app/src/main/java/com/gela/clipsearcher/MainActivity.java
+++ clipsearcher/app/src/main/java/com/gela/clipsearcher/MainActivity.java
@@ -4,28 +4,56 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button;
+    Button searchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.searchbutton);
+        EditText searchText = findViewById(R.id.searchBox);
+        searchButton = findViewById(R.id.searchbutton);
+        searchButton.setEnabled(false);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                System.out.println("button clicked!");
                 Intent intent;
-                System.out.println("intent declared!");
                 intent = new Intent(MainActivity.this, searchActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        // Listener to disable and enable the search button depending on the text input
+        searchText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // Not needed for this implementation
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Check if EditText is empty or not
+                if (s.toString().trim().length() > 0) {
+                    // Enable the button if EditText is not empty
+                    searchButton.setEnabled(true);
+                } else {
+                    // Disable the button if EditText is empty
+                    searchButton.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                //N/A
             }
         });
     }
--- clipsearcher/app/src/main/java/com/gela/clipsearcher/searchActivity.java
+++ clipsearcher/app/src/main/java/com/gela/clipsearcher/searchActivity.java
@@ -2,22 +2,36 @@ package com.gela.clipsearcher;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.EditText;
 
 public class searchActivity extends AppCompatActivity {
 
     WebView webView;
+    EditText searchBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
+        //Get a hold of main layout to get to the search box
+        View mainLayout = getLayoutInflater().inflate(R.layout.activity_main, null);
+        searchBox = mainLayout.findViewById(R.id.searchBox);
+
         webView = findViewById(R.id.searchWebView);
         webView.setWebViewClient(new WebViewClient());
 
-        webView.loadUrl("https://www.google.com/");
+        /// TODO Security concerns about malicious injections here so we should trim the input
+        /// TODO What if whatever is in the clipboard is a media and not text? Ignore in that case?
+        String query = searchBox.getText().toString().trim();
+        if (!query.isEmpty()) {
+            webView.loadUrl("https://www.duckduckgo.com/" + query);
+        }
     }
 }
